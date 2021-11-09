c105143ca5c1eca0f84fdf4207703ed93232261e
mHavardSedeno <mathilde.havard---sedeno@etu.univ-nantes.fr>
2021-11-09 17:33:19 +0100

    pathFollowing - end

--- pathFollowing.py
+++ pathFollowing.py
@@ -1,8 +1,8 @@
-import math, robot
+import math, robot, commands
 import numpy as np
 from matplotlib import pyplot
 
-r=robot.FiveBars([-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8, 17.8],0,2, eps_cmd=20)
+r=robot.FiveBars([-22.5, 0, 22.5, 0, 17.8, 17.8, 17.8, 17.8],0,2, eps_cmd=1)
 
 # draw the path to follow -- a circle centered at (0, −20) with radius 5 -- in dotted line
 centre_x = 0
@@ -15,13 +15,13 @@ r.refresh()
 
 
 # Discretize t in N points
-N = 10
+N = 100
 # t in [0,2pi]
 t = []
 nextT = 0
 
 for i in range(N):
-    nextT += 0.2*math.pi
+    nextT += 1/N *2*math.pi
     t.append(nextT)
 
 print(t)
@@ -33,4 +33,24 @@ for i in range(len(t)):
     pose_y  = centre_y + (radius * np.sin(t[i]))
     poses.append([pose_x, pose_y])
 
-print(poses)
+print(poses[0])
+
+cmds = commands.find_commands(poses)
+print(cmds)
+
+# We go to the first pose
+r.actuate(cmds[0])
+# and start drawing
+r.pen_down()
+
+# then we go through all the poses
+for (x,y) in cmds:
+    r.actuate([x,y])
+
+# and return to the beginning of the circle
+r.actuate(cmds[0])
+r.pen_up()
+input("Press [ENTER] to continue ...")
+
+# and to the initial position
+r.go_home()
